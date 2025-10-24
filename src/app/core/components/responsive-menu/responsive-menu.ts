5e9eb1e2566e9dffd185f52312867fe9865656b4
AldayDev <aldaydev@gmail.com>
2025-10-24 02:41:03 +0200

    feat: ng-menu-toggle addded #14 (#17)

--- src/app/core/components/responsive-menu/responsive-menu.ts
+++ src/app/core/components/responsive-menu/responsive-menu.ts
@@ -2,11 +2,11 @@ import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core
 import { CatalogItem } from '@app/core/data/data.types';
 import { MenuService } from '@app/core/services/menu-service/menu-service';
 import { LinkButton } from '@app/shared/ui/link-button/link-button';
-import { NgMenuButton } from 'ng-menu-button';
+import { NgMenuToggle } from 'ng-menu-toggle';
 
 @Component({
   selector: 'app-responsive-menu',
-  imports: [LinkButton, NgMenuButton],
+  imports: [LinkButton, NgMenuToggle],
   templateUrl: './responsive-menu.html',
   styleUrl: './responsive-menu.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
