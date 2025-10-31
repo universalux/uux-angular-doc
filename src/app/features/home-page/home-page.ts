e8abee5a696eb7a3a48bff966d08f9af92eb7bd5
aldaydev <aldaydev@gmail.com>
2025-10-31 18:25:56 +0100

    feat: divider reusable component created #21

--- src/app/features/home-page/home-page.ts
+++ src/app/features/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HomeHero } from './home-hero/home-hero';
+import { Divider } from "@app/shared/ui/divider/divider";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeHero],
+  imports: [HomeHero, Divider],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
