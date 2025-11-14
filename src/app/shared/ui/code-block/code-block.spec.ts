9b45c2a50202bdf7b4196201c7ed7aadbc9d19c0
aldaydev <aldaydev@gmail.com>
2025-11-14 14:00:15 +0100

    feat: code-block component finished #50

--- src/app/shared/ui/code-block/code-block.spec.ts
+++ src/app/shared/ui/code-block/code-block.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CodeBlock } from './code-block';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('CodeBlock', () => {
   let component: CodeBlock;
@@ -8,7 +9,8 @@ describe('CodeBlock', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CodeBlock]
+      imports: [CodeBlock],
+      providers: [provideZonelessChangeDetection()]
     })
     .compileComponents();
 
