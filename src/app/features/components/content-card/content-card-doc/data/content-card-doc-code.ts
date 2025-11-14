3c076f405077c8e3ab436617f6e5f5bec3b41955
aldaydev <aldaydev@gmail.com>
2025-11-14 21:31:45 +0100

    feat: ng-content-card doc component done #58

---
+++ src/app/features/components/content-card/content-card-doc/data/content-card-doc-code.ts
@@ -0,0 +1,87 @@
+export const contentCardDocCode = {
+  installationCode_1: {
+    language: 'bash',
+    code: `npm install ng-content-card`,
+  },
+  installationCode_2: {
+    language: 'bash',
+    code: `npm install ng-content-card@v19-lts`,
+  },
+  installationCode_3: {
+    language: 'bash',
+    code: `npm install ng-content-card@v18-lts`,
+  },
+  overviewCode_1: {
+    language: 'html',
+    code: `
+    <ng-content-card hover="both">
+        <header card-header>
+          Header Content
+        </header>
+        <section card-image>
+          <img class="scaleHover" src="images/card/dog_post.png" alt="Dog">
+        </section>
+        <section card-body>
+          Body content
+        </section>
+        <footer card-footer>
+          Footer content
+        </footer>
+        <div absolute-content>
+          Extra content
+        </footer>
+      </ng-content-card>
+    `,
+  },
+  funcCode_1: {
+    language: 'html',
+    code: `
+    <section card-image>
+        <img class="scaleHover" src="images/card/dog_post.png" alt="Dog">
+      </section>
+    `,
+  },
+  stylingCode_1: {
+    language: 'css',
+    code: `
+    ng-content-card {
+        max-width: 280px;
+        min-height: 400px;
+        border: 3px solid white;
+        border-radius: 3rem;
+      }
+    `
+  },
+  stylingCode_2: {
+    language: 'css',
+    code: `
+    ng-content-card {
+        --card-bg: white;
+        --card-fg: black;
+      }
+    `
+  },
+  stylingCode_3: {
+    language: 'css',
+    code: `
+    ng-content-card [card-header] {
+        display: flex;
+        align-items: center;
+        column-gap: 1rem;
+      }
+
+      ng-content-card [card-header] img {
+        width: 50px;
+        border-radius: 50%;
+      }
+
+      ng-content-card [card-footer] {
+        display: flex;
+        column-gap: 0.5rem;
+        justify-content: end;
+      }
+    `
+  }
+} as const;
+
+export type ContentCardDocCode = typeof contentCardDocCode;
