28c31a2c59784cd446305e156f10ebe2378b8d01
AldayDev <aldaydev@gmail.com>
2025-11-20 14:16:47 +0100

    87 create contribute page and routes (#91)
    * feat: contribute page almost done #87
    
    * feat: contribute page basic design done #87

---
+++ src/app/features/contribute-page/contribute-page.ts
@@ -0,0 +1,68 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { NgContentCard } from 'ng-content-card';
+
+type ContributeOptions = {
+  label: string;
+  url: string;
+  icon: string;
+  alt: string;
+};
+
+type Repositories = {
+  name: string;
+  description: string;
+  url: string;
+}
+
+
+@Component({
+  selector: 'app-contribute-page',
+  imports: [NgContentCard],
+  templateUrl: './contribute-page.html',
+  styleUrl: './contribute-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ContributePage {
+
+  contributeOptions = signal<ContributeOptions[]>([
+    {
+      label: 'Report a bug',
+      url: 'https://github.com/universalux/uux-hub/issues/new?assignees=aldaydev&labels=bug&template=angular_bug_report.yml',
+      icon: 'images/icons/bug.svg',
+      alt: 'Bug icon'
+    },
+    {
+      label: 'Request an improvement for an existing component',
+      url: 'https://github.com/universalux/uux-hub/issues/new?assignees=aldaydev&labels=bug&template=angular_feature_request.yml',
+      icon: 'images/icons/lightbulb.svg',
+      alt: 'Lightbulb icon'
+    },
+    {
+      label: 'Suggest a new component',
+      url: 'https://github.com/universalux/uux-hub/issues/new?assignees=aldaydev&labels=bug&template=new_component_request.yml',
+      icon: 'images/icons/bolt.svg',
+      alt: 'Bolt icon'
+    }
+  ]);
+
+  repositories = signal<Repositories[]>([
+    {
+      name: 'uux-angular-doc',
+      description: 'Documentation and interactive playgrounds for the Universal UX Angular component library (this app).',
+      url: 'https://github.com/universalux/uux-angular-doc'
+    },
+    {
+      name: 'uux-angular',
+      description: 'Repository for developing Angular UI components for Universal UX (UUX) – includes source code, tests, and build setup.',
+      url: 'https://github.com/universalux/uux-angular'
+    },
+    {
+      name: 'uux-hub',
+      description: 'Central hub for community feedback, assets and visual showcases for the UUX component ecosystem (issues, gifs, screenshots, docs resources).',
+      url: 'https://github.com/universalux/uux-hub'
+    },
+  ]);
+
+  currentDescription = signal<string | null>(null);
+
+}
