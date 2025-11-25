b239e3f332f0c4c5ac0674166fe5a5451f9f172a
AldayDev <aldaydev@gmail.com>
2025-11-25 06:12:40 +0100

    feat: ng-expand-group checked and home faw created #123 (#124)

--- src/app/features/home-page/home-page.ts
+++ src/app/features/home-page/home-page.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HomeHero } from './home-hero/home-hero';
 import { Divider } from "@app/shared/ui/divider/divider";
 import { HomeFeatComponents } from "./home-feat-components/home-feat-components";
 import { HomeFeatKits } from "./home-feat-kits/home-feat-kits";
+import { HomeFaq } from "./home-faq/home-faq";
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomeHero, Divider, HomeFeatComponents, HomeFeatKits],
+  imports: [HomeHero, Divider, HomeFeatComponents, HomeFeatKits, HomeFaq],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
